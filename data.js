7b3103fe28136b4e327a8ecf03c18ffa2732a3cc
Racso <oscarhs0576@gmail.com>
2025-09-01 12:08:32 -0400

    Update data.js

--- data.js
+++ data.js
@@ -1,7 +1,7 @@
 export const tweetsData = [   
     {
         handle: `@TrollBot66756542 💎`,
-        profilePic: `main/troll.jpg`,
+        profilePic: `main/images/troll.jpg`,
         likes: 27,
         retweets: 10,
         tweetText: `Buy Bitcoin, ETH Make 💰💰💰 low low prices. 
